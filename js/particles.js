223397600433578ed6eb6dffb5481bd9030e3059
fradely code <fradelydventura@gmail.com>
2020-06-03 07:59:21 -0400

    sliders

--- js/particles.js
+++ js/particles.js
@@ -1539,9 +1539,6 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
 /* Author : Vincent Garreau  - vincentgarreau.com
 /* MIT license: http://opensource.org/licenses/MIT
 /* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
-/* v2.0.0
 /* ----------------------------------------------- */
 
 // Fradely Code
--- js/script.js
+++ js/script.js
@@ -1,47 +1,64 @@
-/*Fradely Code 2020*/ 
+/**
+ * /*Fradely Code 2020
+ *
+ * @format
+ */
 
 const handleFirstTab = (e) => {
-    if(e.key === 'Tab') {
-        document.body.classList.add('user-is-tabbing')
+	if (e.key === "Tab") {
+		document.body.classList.add("user-is-tabbing");
 
-        window.removeEventListener('keydown', handleFirstTab)
-        window.addEventListener('mousedown', handleMouseDownOnce)
-    }
-}
+		window.removeEventListener("keydown", handleFirstTab);
+		window.addEventListener("mousedown", handleMouseDownOnce);
+	}
+};
 
 const handleMouseDownOnce = () => {
-    document.body.classList.remove('use-is-tabbing')
+	document.body.classList.remove("use-is-tabbing");
 
-    window.removeEventListener('mousedown', handleMouseDownOnce)
-    window.addEventListener('keydown', handleFirstTab)
-}
+	window.removeEventListener("mousedown", handleMouseDownOnce);
+	window.addEventListener("keydown", handleFirstTab);
+};
 
-window.addEventListener('keydown', handleFirstTab)
+window.addEventListener("keydown", handleFirstTab);
 
-/* Start Button */ 
+/* Start Button */
+
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
 
 let alterStyles = (isBackToTopRendered) => {
-    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-    backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
+	backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+	backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+	backToTopButton.style.transform = isBackToTopRendered
+		? "scale(1)"
+		: "scale(0)";
 };
 
-/* Start  Show and hidde button scroll*/ 
+/* Start  Show and hidde button scroll*/
+
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 700) {
-        isBackToTopRendered = true;
-        alterStyles(isBackToTopRendered);
-    }else {
-        isBackToTopRendered = false;
-        alterStyles(isBackToTopRendered);
-    }
+	if (window.scrollY > 700) {
+		isBackToTopRendered = true;
+		alterStyles(isBackToTopRendered);
+	} else {
+		isBackToTopRendered = false;
+		alterStyles(isBackToTopRendered);
+	}
 });
 
-/* Start  Show and hidde button scroll*/ 
-
+/* Start  Show and hidde button scroll*/
 
 /* End Button */
 
-/*Fradely Code 2020*/ 
+/* --------------------------------------------------- */
+/* -------------------STARTLID------------------------ */
+/* --------------------------------------------------- */
+
+
+
+/* --------------------------------------------------- */
+/* -------------------END SLID------------------------ */
+/* --------------------------------------------------- */
+
+/*Fradely Code 2020*/
